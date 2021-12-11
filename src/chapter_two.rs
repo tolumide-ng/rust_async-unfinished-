e41b616e8878b3d64f2b8e7a6316fa3949f3bedc
tolumide-ng <tolumideshopein@gmail.com>
2021-12-11 17:41:47 +0100

    creates a simple timerfuture for the future trait

--- src/chapter_two.rs
+++ src/chapter_two.rs
@@ -2,3 +2,4 @@ pub mod future_trait;
 pub mod socket;
 pub mod state_machine;
 pub mod sequential_futures;
+pub mod timer_future;
--- src/chapter_two/future_trait.rs
+++ src/chapter_two/future_trait.rs
@@ -1,10 +1,14 @@
+use std::pin::Pin;
+use std::task::Context;
+
 pub enum Poll<T> {
     Ready(T),
     Pending,
 }
+
 pub trait SimpleFuture {
     type Output;
-    fn poll(&mut self, wake: fn()) -> Poll<Self::Output>;
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output>;
 }
 
 
